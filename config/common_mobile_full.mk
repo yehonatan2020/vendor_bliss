240a3f8878b88238af342f1986edfd7c290c12bd
yehonatan2020 <yehonatan@yandex.ru>
2024-11-08 10:59:36 +0000

    Remove recorder

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -3,10 +3,6 @@ $(call inherit-product, vendor/bliss/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Recorder
-PRODUCT_PACKAGES += \
-    Recorder
-
 # Include Bliss LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/bliss/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/bliss/overlay/dictionaries
