1902b4b720c31124f89422546afb5c1b1dff35c5
yehonatan2020 <yehonatan@yandex.ru>
2024-11-08 11:10:36 +0000

    some minor change

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,7 +1,5 @@
 # GMS
-ifeq ($(WITH_GMS),true)
 WITH_GMS_COMMS_SUITE := true
-endif
 
 # Sensitive Phone Numbers list
 PRODUCT_PACKAGES += \
@@ -17,7 +15,7 @@ PRODUCT_PACKAGES += \
 
 # Default ringtone
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Orion.ogg
+    ro.config.ringtone=Your_new_adventure.ogg
 
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
